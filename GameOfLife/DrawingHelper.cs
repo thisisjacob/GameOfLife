d2d7019fa43f255b7c163ceb466925bad976fb00
Jacob H <jacobholfelder@gmail.com>
2020-04-05 21:41:56 -0400

    Add project files.

---
+++ GameOfLife/DrawingHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GameOfLife
+{
+    public static class DrawingHelper
+    {
+        public static void TestDraw(Canvas surface)
+        {
+            System.Windows.Shapes.Rectangle test = new System.Windows.Shapes.Rectangle();
+            //test.Stroke = new Brushes.Black;
+            //test.Fill = new Brushes.SkyBlue;
+        }
+    }
+}
