50c4f801d93e01b1b02592ac0621241c2b538173
thisisjacob <jacobholfelder@gmail.com>
2020-06-24 21:49:40 -0400

    Preparation for adding importing/exporting of games

---
+++ GameOfLife/FileManagement/GameStateFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace GameOfLife.FileManagement
+{
+	public class GameStateFile
+	{
+		// Only valid constructor. For saving or reading from file
+		GameStateFile(GameState game, LifeRuleset rules, string file)
+		{
+
+		}
+	}
+}
---
+++ GameOfLife/Windows/NotificationWindows/FileWriteError.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameOfLife.Windows.NotificationWindows
+{
+	/// <summary>
+	/// Interaction logic for FileWriteError.xaml
+	/// </summary>
+	public partial class FileWriteError : Window
+	{
+		public FileWriteError()
+		{
+			InitializeComponent();
+		}
+	}
+}
