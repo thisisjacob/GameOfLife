dc0b38a7bc32ea1d997c03dd9e33fb3de5b03aa9
thisisjacob <jacobholfelder@gmail.com>
2020-06-24 22:13:42 -0400

    Basic function

--- GameOfLife/FileManagement/GameStateFile.cs
+++ GameOfLife/FileManagement/GameStateFile.cs
@@ -7,10 +7,16 @@ namespace GameOfLife.FileManagement
 {
 	public class GameStateFile
 	{
+		GameState gameInfo { get; set; }
+		LifeRuleset rulesInfo { get; set; }
+		string fileName { get; set; }
+
 		// Only valid constructor. For saving or reading from file
 		GameStateFile(GameState game, LifeRuleset rules, string file)
 		{
-
+			gameInfo = game;
+			rulesInfo = rules;
+			fileName = file;
 		}
 	}
 }
---
+++ GameOfLife/FileManagement/GameStateFileReadWrite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife.FileManagement
+{
+	public static class GameStateFileReadWrite
+	{
+		public static void WriteToFile(GameStateFile data)
+		{
+
+		}
+
+		/*
+		public static GameStateFile ReadFromFile(string file)
+		{
+
+		}
+		*/
+	}
+}
