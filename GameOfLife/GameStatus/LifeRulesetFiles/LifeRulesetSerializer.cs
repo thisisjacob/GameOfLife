ee36d5373b5e7249f76d62e5d22f7c8efadd7f2f
thisisjacob <jacobholfelder@gmail.com>
2020-07-13 19:44:52 -0400

    Created a class for serialization interaction with LifeRuleset

---
+++ GameOfLife/GameStatus/LifeRulesetFiles/LifeRulesetSerializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace GameOfLife.GameStatus.LifeRulesetFiles
+{
+	class LifeRulesetSerializer : ISerializable
+	{
+		public int[] NeighborsToGrow;
+		public int[] NeighborsToLive;
+		public int[] NeighborsToDie;
+
+		public LifeRulesetSerializer()
+		{
+
+		}
+
+		public LifeRulesetSerializer(LifeRuleset rules)
+		{
+			NeighborsToGrow = rules.GetGrowthArray();
+			NeighborsToLive = rules.GetLivingArray();
+			NeighborsToDie = rules.GetDeathArray();
+		}
+
+		public LifeRulesetSerializer(SerializationInfo info, StreamingContext context)
+		{
+			NeighborsToGrow = (int[])info.GetValue("NeighborsToGrow", typeof(int[]));
+			NeighborsToLive = (int[])info.GetValue("NeighborsToLive", typeof(int[]));
+			NeighborsToDie = (int[])info.GetValue("NeighborsToDie", typeof(int[]));
+		}
+
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue("NeighborsToGrow", NeighborsToGrow, typeof(int[]));
+			info.AddValue("NeighborsToLive", NeighborsToLive, typeof(int[]));
+			info.AddValue("NeighborsToDie", NeighborsToDie, typeof(int[]));
+		}
+	}
+}
